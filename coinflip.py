0df4165b2e1c33bcab9a8b6bf6aaafca22a8dbdc
Parker Wignall <parkerw444@gmail.com>
2026-06-19 19:58:33 -0700

    Added new title screen using ASCII art generated from https://www.asciiart.eu/text-to-ascii-art

--- coinflip.py
+++ coinflip.py
@@ -1,7 +1,19 @@
 import random
 import time
 def main():
-    answer = input("Heads or Tails?")
+    print(" _  _             _                _____     _ _          ")
+    print("| || |___ __ _ __| |___  ___ _ _  |_   _|_ _(_) |___      ")
+    print("| __ / -_) _` / _` (_-< / _ \ '_|   | |/ _` | | (_-<      ")
+    print("|_||_\___\__,_\__,_/__/ \___/_|    _|_|\__,_|_|_/__/  _ _ ")
+    print("| _ \__ _ _ _| |_____ _ _  \ \    / (_)__ _ _ _  __ _| | |")
+    print("|  _/ _` | '_| / / -_) '_|  \ \/\/ /| / _` | ' \/ _` | | |")
+    print("|_| \__,_|_| |_\_\___|_|     \_/\_/ |_\__, |_||_\__,_|_|_|")
+    print("                                      |___/               ")
+    
+    
+    print("Heads or Tails?")
+    answer = input("--> ")
+    
     verdict = random.randint(0, 2)
     
     if(answer.lower() != "heads" and answer.lower() != "tails"):
